1509a5a5d8e8e7ecb962e73ea1365d29632b9d55
Luke Stoward <luke.stoward@students.plymouth.ac.uk>
2015-12-16 20:37:31 +0000

    Swapped ngRoute for ui.router.
    Fixes #1

--- Twitter Dashboard/Scripts/app/app.js
+++ Twitter Dashboard/Scripts/app/app.js
@@ -1,19 +1,40 @@
 ﻿(function () {
 
     //Create our angular module (with dependencies)
-    var app = angular.module("twitterDashboard", ["ngRoute"]);
+    //var app = angular.module("twitterDashboard", ["ngRoute"]);
 
-    //Configure routing
-    app.config(function($routeProvider, $locationProvider) {
-        $routeProvider
-            .when("/", {
-                templateUrl: "main.html"
-            })
-            .when("/repo/:username/:reponame", {
-                templateUrl: "repoDetails.html"
-            })
-            .otherwise({ redirectTo: "/main" });
+    //var version = "?v1.0";
 
-        $locationProvider.html5Mode(true);
+    ////Configure routing
+    //app.config(function($routeProvider, $locationProvider) {
+    //    $routeProvider
+    //        .when("/", {
+    //            templateUrl: "main.html?" + version
+    //        })
+    //        .when("/repo/:username/:reponame", {
+    //            templateUrl: "repoDetails.html" + version,
+    //            controller: "RepoDetailsController"
+    //        })
+    //        .otherwise({ redirectTo: "/" });
+
+    //    $locationProvider.html5Mode(true);
+    //});
+
+    var app = angular.module("twitterDashboard", ["ui.router"]);
+
+
+    app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+        $stateProvider
+          .state('Home', {
+              url: "/",
+              templateUrl: "main.html"
+          })
+          .state('RepoDetails', {
+              url: "/repo/{username}/{reponame}",
+              templateUrl: "repoDetails.html",
+              controller: 'RepoDetailsController'
+          });
+        $urlRouterProvider.otherwise("/");
     });
 }());
